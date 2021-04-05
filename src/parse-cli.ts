53ac15626132fd5390888bc7ed0c3b90650d08a8
Michael Hadley <michaelwesthadley@gmail.com>
2021-04-05 15:35:27 -0500

    Update cli parsing to normalize paths and make output path optional

--- src/parse-cli.ts
+++ src/parse-cli.ts
@@ -1,7 +1,7 @@
 import { Command } from "commander";
 import { isFileSync } from "./file-utils";
 import logger from "./logger";
-import { parse } from "path";
+import { normalize, parse, format, FormatInputPathObject } from "path";
 
 export default function parseCli() {
   const program = new Command();
@@ -10,10 +10,11 @@ export default function parseCli() {
   let outputPath = "";
   program
     .version("1.0.1")
-    .arguments("<pathToStudentWorkZip> <pathToOutputUnzippedWork>")
+    .arguments("<pathToStudentWorkZip> [pathToOutputUnzippedWork]")
     .description("canvas-unzipper", {
       pathToStudentWorkZip: "Path to a downloaded submissions zip from Canvas",
-      pathToOutputUnzippedWork: "Path to output the unzipped and organized student work",
+      pathToOutputUnzippedWork:
+        "Path to output the unzipped and organized student work. If not specified, this outputs to a folder at pathToStudentWorkZip (minus the .zip extension).",
     })
     .option("-v, --verbose", "Output extra verbose information while unzipping student work.")
     .action((inPath, outPath, options) => {
@@ -23,10 +24,22 @@ export default function parseCli() {
 
   program.parse(process.argv);
 
-  const isZip = parse(studentWorkZip).ext === ".zip";
+  studentWorkZip = normalize(studentWorkZip);
+  const parsedPath = parse(studentWorkZip);
+  const isZip = parsedPath.ext === ".zip";
   if (!studentWorkZip || !isFileSync(studentWorkZip) || !isZip) {
     logger.error("You need to provide a valid path to a zip of student work from Canvas.");
     process.exit(1);
+  }
+
+  if (!outputPath) {
+    // Exclude the base and ext so that we get a path without the extension.
+    const outPathObj: FormatInputPathObject = {
+      root: parsedPath.root,
+      dir: parsedPath.dir,
+      name: parsedPath.name,
+    };
+    outputPath = format(outPathObj);
   }
 
   const options = program.opts();
